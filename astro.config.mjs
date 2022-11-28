c28962de27bc86a375394a21a05608da1cbc0094
Viktor Glushak <v.yu.glushak@tmn2.etagi.com>
2022-11-29 00:05:17 +0500

    deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,8 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
+  site: 'https://viktorglushak.github.io',
+  base: '/astro-overview',
 });
+
